dcdc2bf2eaf16d1a636aa367bd8f91e6838cc2d3
osawa-koki <github@osawa.dev>
2023-02-19 23:51:24 +0900

    変数。

--- src/main.cbl
+++ src/main.cbl
@@ -1,8 +1,15 @@
            IDENTIFICATION DIVISION.
            PROGRAM-ID. HELLO-WORLD.
 
+           DATA DIVISION.
+           WORKING-STORAGE SECTION.
+           01 A PIC 9(4) VALUE 100.
+           01 B PIC 9(4) VALUE 200.
+
            PROCEDURE DIVISION.
 
-                 DISPLAY "Hello, World!".
+           DISPLAY "Hello, World!".
+           DISPLAY "A = " A.
+           DISPLAY "B = " B.
 
-                 STOP RUN.
+           STOP RUN.
