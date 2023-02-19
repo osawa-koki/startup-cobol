a2a5cd90216329a973e3b758a27baa04f1a3bef4
osawa-koki <github@osawa.dev>
2023-02-20 00:00:02 +0900

    標準入力。

--- src/main.cbl
+++ src/main.cbl
@@ -6,6 +6,8 @@
            01 A PIC 9(4) VALUE 100.
            01 B PIC 9(4) VALUE 200.
 
+           01 NUM PIC 9(4).
+
            PROCEDURE DIVISION.
 
            *> 標準出力
@@ -16,5 +18,10 @@
            *> 和を計算して表示
            ADD A TO B
            DISPLAY "A + B = " B.
+
+           *> 標準入力を受け取る
+           DISPLAY "Input a number: ".
+           ACCEPT NUM.
+           DISPLAY "Input number is " NUM.
 
            STOP RUN.
